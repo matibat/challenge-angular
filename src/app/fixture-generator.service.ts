056d0a3d99f84beaebe0db236e28cf4bb962fbed
Mati <matibat@gmail.com>
2018-11-13 02:21:35 -0300

    fix FixtureGeneratorService logic

--- src/app/fixture-generator.service.ts
+++ src/app/fixture-generator.service.ts
@@ -9,12 +9,73 @@ export class FixtureGeneratorService {
   // private const MIN_TEAMS_QUANTITY = 4;
   // private const MAX_TEAMS_QUANTITY = 20;
 
-  private teams: Array<any> = [];
+  private teams: Array<any> = [ // todo: femove fake data
+    {id: 0, name: 'batman'},
+    {id: 1, name: 'mati'},
+    {id: 2, name: 'cacho'},
+    {id: 3, name: 'pepe'},
+  ];
   private nextTeamId = 0;
   private teamsObservers: Array<Observer<any>> = [];
 
   constructor() {
     this.noRepeatNameValidator = this.noRepeatNameValidator.bind(this);
+  }
+
+  private getMatchesSortedByDay(): Array<any> { // todo: re-write me completely (-:
+    const sortedMatches: Array<any> = [];
+    for (let dayIndex = 0; dayIndex < this.totalOfRounds(); dayIndex++) {
+      sortedMatches.push(this.getMatchesForDay(dayIndex));
+    }
+    return sortedMatches;
+  }
+
+  private getMatchesForDay(dayIndex: number): Array<string> {
+    const matchesForDay = [];
+    const lastTeamIndex = this.teams.length - 1;
+    let match = [1, lastTeamIndex - dayIndex + 1];
+    for (let matchIndex = 0; matchIndex < this.totalOfMatchesByDay(); matchIndex++) {
+      matchesForDay.push(match);
+      match = this.getParallelMatch(match);
+    }
+    return matchesForDay;
+  }
+
+  private getParallelMatch(match: Array<number>): Array<number> {
+    const parallelMatch: Array<number> = [];
+    if (match[0] === 1) {
+      parallelMatch[0] = this.cyclicSum(match[1], 1);
+      parallelMatch[1] = this.cyclicSum(match[1], -1);
+    } else {
+      parallelMatch[0] = this.cyclicSum(match[0], +1);
+      parallelMatch[1] = this.cyclicSum(match[1], -1);
+    }
+    return parallelMatch;
+  }
+
+  private cyclicSum(a: number, b: number): number {
+    let sum: number;
+    if (b > 0) {
+      sum = (a + b) % (this.teams.length + 1);
+      if (this.isLastTeamOfCycle(a)) { sum = 2; }
+    } else {
+      // if (a === 2) debugger;
+      sum = (a + b) % (this.teams.length + 1);
+      if (this.isFirstTeamOfCycle(sum)) { sum = this.teams.length; }
+    }
+    return sum;
+  }
+
+  private isFirstOrLastTeamIndex(index: number): boolean {
+    return index === 0 || index === 1 || index === 2 || index === this.teams.length;
+  }
+
+  private isFirstTeamOfCycle(index: number) {
+    return index < 2;
+  }
+
+  private isLastTeamOfCycle(index: number) {
+    return index === this.teams.length;
   }
 
   private isDataReady(): boolean {
@@ -31,36 +92,12 @@ export class FixtureGeneratorService {
     return false;
   }
 
-  private getMatchesSortedByDay(): Array<any> { // todo: re-write me completely (-:
-    const sortedMatches: Array<any> = [];
-    for (let dayIndex = 0; dayIndex < this.totalOfMatchesByDay(); dayIndex++) {
-      sortedMatches.push(this.getMatchesForDay(dayIndex));
-    }
-    return sortedMatches;
-  }
-
-  private getMatchesForDay(dayIndex: number): Array<string> {
-    const matchesForDay = [];
-    const lastTeamIndex = this.teams.length - 1;
-    let match = [1, lastTeamIndex - dayIndex];
-    for (let matchIndex = 0; matchIndex < this.totalOfMatchesByDay(); matchIndex++) {
-      matchesForDay.push(match);
-      match = this.getParallelMatch(match);
-    }
-    return matchesForDay;
-  }
-
-  private getParallelMatch(match: Array<number>): Array<number> {
-    const parallelMatch = Number[2];
-    parallelMatch[0] = (match[0] + 1) % this.teams.length;
-    parallelMatch[1] = (match[1] - 1) % this.teams.length;
-    if (parallelMatch[0] === 0) { parallelMatch[0] = 2; }
-    if (parallelMatch[1] === 0) { parallelMatch[1] = 2; }
-    return parallelMatch;
-  }
-
   private totalOfMatchesByDay(): number {
     return this.teams.length / 2;
+  }
+
+  private totalOfRounds(): number {
+    return this.teams.length - 1;
   }
 
   private handleTeamsUpdate(observer: Observer<any>) {
