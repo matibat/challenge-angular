fc79f75c84d406c250055826510eeafa9af6b8d6
Mati <matibat@gmail.com>
2018-11-12 23:25:08 -0300

    finish basic structure of service

--- src/app/fixture-generator.service.ts
+++ src/app/fixture-generator.service.ts
@@ -11,7 +11,7 @@ export class FixtureGeneratorService {
 
   private teams: Array<any> = [];
   private nextTeamId = 0;
-  private teamsObservers: Observer<any>[] = [];
+  private teamsObservers: Array<Observer<any>> = [];
 
   constructor() {
     this.noRepeatNameValidator = this.noRepeatNameValidator.bind(this);
@@ -31,40 +31,36 @@ export class FixtureGeneratorService {
     return false;
   }
 
-  private getIndexFromPositionInFixture() {
-
-  }
-
-  private getFixtureStructure() {
-    const structure: Array<any> = [];
-
-    const numberOfDates = this.teams.length - 1;
-    const matchesPerDate = this.teams.length / 2;
-    for (let dateIndex = 0; dateIndex < numberOfDates; dateIndex++) {
-      for (let matchIndex = 0; matchIndex <= matchesPerDate; matchIndex++) {
-        structure[dateIndex].push([ ]);
-      }
-    }
-
-    return structure;
-  }
-
-  private getMatchesArray(): Array<any> {
-    const matches: Array<any> = [];
-    const lastTeamIndex = this.teams.length - 1;
-
-    for (let team1Index = 0; team1Index < lastTeamIndex - 1; team1Index++) {
-      for (let team2Index = team1Index + 1; team2Index < lastTeamIndex; team2Index++) {
-        matches.push([team1Index, team2Index]);
-      }
-    }
-
-    return matches;
-  }
-
   private getMatchesSortedByDay(): Array<any> { // todo: re-write me completely (-:
     const sortedMatches: Array<any> = [];
+    for (let dayIndex = 0; dayIndex < this.totalOfMatchesByDay(); dayIndex++) {
+      sortedMatches.push(this.getMatchesForDay(dayIndex));
+    }
     return sortedMatches;
+  }
+
+  private getMatchesForDay(dayIndex: number): Array<string> {
+    const matchesForDay = [];
+    const lastTeamIndex = this.teams.length - 1;
+    let match = [1, lastTeamIndex - dayIndex];
+    for (let matchIndex = 0; matchIndex < this.totalOfMatchesByDay(); matchIndex++) {
+      matchesForDay.push(match);
+      match = this.getParallelMatch(match);
+    }
+    return matchesForDay;
+  }
+
+  private getParallelMatch(match: Array<number>): Array<number> {
+    const parallelMatch = Number[2];
+    parallelMatch[0] = (match[0] + 1) % this.teams.length;
+    parallelMatch[1] = (match[1] - 1) % this.teams.length;
+    if (parallelMatch[0] === 0) { parallelMatch[0] = 2; }
+    if (parallelMatch[1] === 0) { parallelMatch[1] = 2; }
+    return parallelMatch;
+  }
+
+  private totalOfMatchesByDay(): number {
+    return this.teams.length / 2;
   }
 
   private handleTeamsUpdate(observer: Observer<any>) {
@@ -77,6 +73,8 @@ export class FixtureGeneratorService {
       this.handleTeamsUpdate(observer);
     }
   }
+
+  // PUBLIC ACCESS MEMBERS
 
   public getTeams(): Observable<any> {
     return new Observable((observer) => {
